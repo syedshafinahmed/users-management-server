8c97dd05325902f728a06537119fc6935a1c1d87
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-10-28 16:11:37 +0600

    Use fetch to load data

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+
+
+app.use(cors());
+
 
 app.get('/', (req, res) => {
   res.send('Users Server is Available');
 })
+
+
+const users = [
+  {id: 1, name: 'Shafin', email:'shafin@gmail.com'},
+  {id: 2, name: 'Nafin', email:'nafin@gmail.com'},
+  {id: 3, name: 'Ahmed', email:'ahmed@gmail.com'},
+  {id: 4, name: 'Dhruv', email:'dhruv@gmail.com'},
+]
+
+
+app.get('/users', (req, res) => {
+  res.send(users);
+})
+
 
 app.listen(port, () => {
   console.log(`Users Server Started on Port: ${port}`)
